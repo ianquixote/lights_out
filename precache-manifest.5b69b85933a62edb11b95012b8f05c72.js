a48a1fbf9226e1c9d183f9e51adef5d5c1396f92
Ian Austin <ianaustin2560@gmail.com>
2022-08-26 13:38:24 -0700

    Updates

---
+++ precache-manifest.5b69b85933a62edb11b95012b8f05c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d13638ee04f3cd10d73",
+    "url": "/lights_out/static/css/main.d269e1a9.chunk.css"
+  },
+  {
+    "revision": "2d13638ee04f3cd10d73",
+    "url": "/lights_out/static/js/main.2d13638e.chunk.js"
+  },
+  {
+    "revision": "703d07fd83ee1bb9f224",
+    "url": "/lights_out/static/js/1.703d07fd.chunk.js"
+  },
+  {
+    "revision": "9f8678a28ec394cd6ac8",
+    "url": "/lights_out/static/js/runtime~main.9f8678a2.js"
+  },
+  {
+    "revision": "5f67cf642703765c8fec780f21c20543",
+    "url": "/lights_out/index.html"
+  }
+];
