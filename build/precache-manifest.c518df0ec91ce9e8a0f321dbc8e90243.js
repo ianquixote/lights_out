e597d64c7c86ee009e80073b0039ca211176be63
Ian Austin <ianaustin2560@gmail.com>
2022-08-26 13:20:43 -0700

    Ran npm run build and added homepage to package.json.

---
+++ build/precache-manifest.c518df0ec91ce9e8a0f321dbc8e90243.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18c2e56eb177cd4a2a62",
+    "url": "/static/css/main.d269e1a9.chunk.css"
+  },
+  {
+    "revision": "18c2e56eb177cd4a2a62",
+    "url": "/static/js/main.18c2e56e.chunk.js"
+  },
+  {
+    "revision": "703d07fd83ee1bb9f224",
+    "url": "/static/js/1.703d07fd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f497e41bdf5787994b11ebda710de4ed",
+    "url": "/index.html"
+  }
+];
