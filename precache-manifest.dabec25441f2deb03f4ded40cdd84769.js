c0e0f938e346127b67db0073e058c546e3c79ae9
Ian Austin <ianaustin2560@gmail.com>
2022-08-26 18:20:53 -0700

    reverting to older version

---
+++ precache-manifest.dabec25441f2deb03f4ded40cdd84769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87cf654a62e1a8690c8f",
+    "url": "/lights_out/static/css/main.e50e1adf.chunk.css"
+  },
+  {
+    "revision": "87cf654a62e1a8690c8f",
+    "url": "/lights_out/static/js/main.87cf654a.chunk.js"
+  },
+  {
+    "revision": "703d07fd83ee1bb9f224",
+    "url": "/lights_out/static/js/1.703d07fd.chunk.js"
+  },
+  {
+    "revision": "9f8678a28ec394cd6ac8",
+    "url": "/lights_out/static/js/runtime~main.9f8678a2.js"
+  },
+  {
+    "revision": "b8b9a0b568f2a6e79d18ed4899fc162e",
+    "url": "/lights_out/index.html"
+  }
+];
